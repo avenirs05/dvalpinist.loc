e373531056bef9cdf7b31a97a43557e7239231da
avenirs05 <avenirs05@gmail.com>
2017-12-05 01:21:38 +0200

    last

---
+++ header.php
@@ -0,0 +1,67 @@
+	<!DOCTYPE html>
+	<html lang="ru-RU">
+	<head>
+	    <meta name="description" content=""/>
+	    <meta name="keywords" content=""/>
+	    <meta name="author" content=""/>
+	    <meta name="copyright" content="(c)">
+		<meta charset="UTF-8">
+	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	    <meta name="format-detection" content="telephone=no"/>
+	    <meta name="format-detection" content="address=no"/>
+	    <meta http-equiv="x-rim-auto-match" content="none">
+	    <meta name="HandheldFriendly" content="True"/>
+	    <meta http-equiv="cleartype" content="on"/>
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	    <meta http-equiv="imagetoolbar" content="no"/>
+	    <meta http-equiv="msthemecompatible" content="no"/>
+	    <meta name="apple-mobile-web-app-capable" content="yes"/>
+	    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+	    
+	    <title>Строительно­-монтажные высотные работы во Владивостоке и Артёме</title>
+	    <link rel='dns-prefetch' href='//s.w.org' />
+        <link rel="alternate" type="application/rss+xml" title="Строительно­-монтажные высотные работы во Владивостоке и Артёме &raquo; Лента комментариев к &laquo;Главная Страница&raquo;" href="http://dvalpinist.ru/sample-page/feed/" />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.2.1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.2.1\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/dvalpinist.ru\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.7.5"}};
+			!function(a,b,c){function d(a){var b,c,d,e,f=String.fromCharCode;if(!k||!k.fillText)return!1;switch(k.clearRect(0,0,j.width,j.height),k.textBaseline="top",k.font="600 32px Arial",a){case"flag":return k.fillText(f(55356,56826,55356,56819),0,0),!(j.toDataURL().length<3e3)&&(k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57331,65039,8205,55356,57096),0,0),b=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57331,55356,57096),0,0),c=j.toDataURL(),b!==c);case"emoji4":return k.fillText(f(55357,56425,55356,57341,8205,55357,56507),0,0),d=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55357,56425,55356,57341,55357,56507),0,0),e=j.toDataURL(),d!==e}return!1}function e(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g,h,i,j=b.createElement("canvas"),k=j.getContext&&j.getContext("2d");for(i=Array("flag","emoji4"),c.supports={everything:!0,everythingExceptFlag:!0},h=0;h<i.length;h++)c.supports[i[h]]=d(i[h]),c.supports.everything=c.supports.everything&&c.supports[i[h]],"flag"!==i[h]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[i[h]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel='stylesheet' id='contact-form-7-css'  href='http://dvalpinist.ru/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=4.7' type='text/css' media='all' />
+<link rel='stylesheet' id='tablepress-default-css'  href='http://dvalpinist.ru/wp-content/plugins/tablepress/css/default.min.css?ver=1.8' type='text/css' media='all' />
+<script type='text/javascript' src='http://dvalpinist.ru/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='http://dvalpinist.ru/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<link rel='https://api.w.org/' href='http://dvalpinist.ru/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://dvalpinist.ru/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://dvalpinist.ru/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 4.7.5" />
+<link rel="canonical" href="http://dvalpinist.ru/" />
+<link rel='shortlink' href='http://dvalpinist.ru/' />
+<link rel="alternate" type="application/json+oembed" href="http://dvalpinist.ru/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fdvalpinist.ru%2F" />
+<link rel="alternate" type="text/xml+oembed" href="http://dvalpinist.ru/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fdvalpinist.ru%2F&#038;format=xml" />
+<link rel="icon" href="http://dvalpinist.ru/wp-content/uploads/2017/04/cropped-logo-32x32.jpg" sizes="32x32" />
+<link rel="icon" href="http://dvalpinist.ru/wp-content/uploads/2017/04/cropped-logo-192x192.jpg" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="http://dvalpinist.ru/wp-content/uploads/2017/04/cropped-logo-180x180.jpg" />
+<meta name="msapplication-TileImage" content="http://dvalpinist.ru/wp-content/uploads/2017/04/cropped-logo-270x270.jpg" />
+	    <link href="https://fonts.googleapis.com/css?family=Oswald:500&amp;subset=cyrillic" rel="stylesheet">
+	    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;subset=cyrillic" rel="stylesheet">
+	    <link rel="stylesheet" type="text/css" href="http://dvalpinist.ru/wp-content/themes/alpinizm/fancybox/jquery.fancybox.css">
+	    <link rel="stylesheet" href="http://dvalpinist.ru/wp-content/themes/alpinizm/style/reset.css">
+	    <link rel="stylesheet" href="http://dvalpinist.ru/wp-content/themes/alpinizm/style/animate.css">
+	    <link rel="stylesheet" href="http://dvalpinist.ru/wp-content/themes/alpinizm/style/slick-theme.css">
+	    <link rel="stylesheet" href="http://dvalpinist.ru/wp-content/themes/alpinizm/style/slick.css">
+	    <link rel="stylesheet" href="http://dvalpinist.ru/wp-content/themes/alpinizm/style/style.css">
+	    <link rel="stylesheet" type="text/css" href="http://dvalpinist.ru/wp-content/themes/alpinizm/style.css">
+	</head>
